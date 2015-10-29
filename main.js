5d1c5e3d12a009312f973d0e2a64171f440d209e
lindseyemaddox <lindseyemaddox@gmail.com>
2015-10-28 21:39:37 -0500

    can't keep my eyes open...

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
 $(document).ready(function(){
 	var body = $('body');
-	var name = $('#name');
 	var avatar = $('.avatar');
+	var login = $('#login');
+	var name = $('#name');
+	var location = $('#location');
+	var created = $('#created');
 
 
 	$.getJSON('https://api.github.com/users/lindseyemaddox').done(function(data) {
-	  	name.html(data.login),
-	  	avatar.attr({
-	  		src: data.avatar_url
-	  	})
+	  	avatar.attr({src: data.avatar_url}),
+	  	login.html(data.login),
+	  	name.html(data.name),
+	  	location.html(data.location),
+	  	created.html(data.created)
+
 
 
 
