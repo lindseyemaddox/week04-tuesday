1cc8e30b4a2e03f2ecae3a6d3b02a8f4ece720d0
lindseyemaddox <lindseyemaddox@gmail.com>
2015-10-28 21:04:48 -0500

    avatar!

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
 $(document).ready(function(){
 	var body = $('body');
 	var name = $('#name');
+	var avatar = $('.avatar');
+
 
 	$.getJSON('https://api.github.com/users/lindseyemaddox').done(function(data) {
-	  name.html(data.login)
+	  	name.html(data.login),
+	  	avatar.attr({
+	  		src: data.avatar_url
+	  	})
+
+
+
 	});	
+
 })
+
+
+
+
 
