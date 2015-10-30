bd73d3c173621e7deb1d4f407d614405861b61d4
lindseyemaddox <lindseyemaddox@gmail.com>
2015-10-30 08:25:38 -0500

    more styling, added some hrefs

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
 	var body = $('body'),
 		avatar = $('.avatar'),
-		login = $('#login'),
-		name = $('#name'),
+		login = $('#login a'),
+		name = $('#name a'),
 		location = $('#location'),
 		created = $('#created'),
 		followers = $('#followers'),
@@ -13,14 +13,15 @@ $(document).ready(function(){
 
 	$.getJSON('https://api.github.com/users/lindseyemaddox').done(function(data) {
 	  	avatar.attr({src: data.avatar_url}),
-	  	name.html(data.name),
 	  	login.html(data.login),
+	  	login.attr({href: data.html_url}),
+	  	name.html(data.name),
+	  	name.attr({href: data.html_url}),
 	  	location.html(data.location),
 	  	created.html(moment(data.created_at).format('ll')),
 	  	followers.html(data.followers),
 	  	starred.attr({href: data.starred_url}),
 	  	following.html(data.following)
-
 
 	});	
 
@@ -29,21 +30,16 @@ $(document).ready(function(){
 
 	$.getJSON('https://api.github.com/users/lindseyemaddox/repos').done(function(data) {
 
-		var updated = moment(data.updated_at).toNow(true);
+		var updated = moment(data.updated_at).toNow(true);											// something's wrong here. Can't muster the time ago deal.
 		var arr = data;
 		var textToInsert = [];
 		var i = 0;
 		var length = arr.length;
 		for (var a = 0; a < length; a += 1) {
-		    textToInsert[i++] = '<div class="repo">';
-		    textToInsert[i++] = '<div class="left"><a href=" ' + arr[a].html_url + '">' + arr[a].name + '</a>' + '<p class="updated">Updated ' + updated + ' ago</p></div><div class="right">' + arr[a].language + '<a href="' + arr[a].stargazers_url + '"><i class="octicon octicon-star"> ' + arr[a].stargazers_count + '</i></a>' + '<a href="' + arr[a].branches_url + '"><i class="octicon octicon-git-branch"> ' + arr[a].forks_count + '</i></a></div><div style="clear:both;"></div>';  // hack alert - can't figure out why omega isn't clearing my neat floats...
-		    textToInsert[i++] = '</div>' ;
+		    textToInsert[i++] = '<div class="repo"><div class="left"><a href=" ' + arr[a].html_url + '">' + arr[a].name + '</a>' + '<p class="updated">Updated ' + updated + ' ago</p></div><div class="right">' + arr[a].language + '<a href="' + arr[a].stargazers_url + '"><i class="octicon octicon-star"> ' + arr[a].stargazers_count + '</i></a>' + '<a href="' + arr[a].branches_url + '"><i class="octicon octicon-git-branch"> ' + arr[a].forks_count + '</i></a></div><div style="clear:both;"></div></div>' ;     // hack alert - can't figure out why omega isn't clearing my neat floats...
 		}
 
 		$('main').append(textToInsert.join(''));
-
-
-
 
 	});		
 
@@ -53,31 +49,17 @@ $(document).ready(function(){
 
 
 
-	// $.getJSON('https://api.github.com/users').done(function(data) {     // 5 hours later, still no icons. I surrender.
-	//   	// organizations.attr({src: data.organizations_url})
+	$.getJSON('https://api.github.com/user/orgs').done(function(data) {     						// 5 hours later, still no icon. I surrender.
 
-	// 	$(function(){
-	// 		firstLoad();
-	// 	});
+		var arr = data;
+		var textToInsert = [];
+		var i = 0;
+		var length = arr.length;
+		for (var a = 0; a < length; a += 1) {
+		    textToInsert[i++] = '<img src="' + arr[a].organizations_url + '" alt="organization">';
+		}
 
-	// 	function firstLoad(){
-	// 		myFunction();
-	// 	}
+		organizations.append(textToInsert.join(''));
 
-	//   	function getData(icons) {
-	//   		var data = [];
-
-	//   		icons.forEach(function(orgs) {
-	//   			data.push(orgs.organizations_url);
-	//   		})
-	  	
-	//   		return data;
-	//   	}
-
-	// 	function myFunction() {
-	// 	    data.push(data.organizations_url);
-	// 	    $("#organizations").html = data;
-	// 	}
-
-	// });	
+	});	
 
